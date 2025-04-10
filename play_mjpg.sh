a3d0ac602ca1f893bd9667d657994d1a5121ccb4
Zulkarnaen <zulkarnaen@tii.ae>
2025-04-10 09:58:58 +0400

    Fixed typo on the expected file name

--- play_mjpg.sh
+++ play_mjpg.sh
@@ -2,4 +2,4 @@
 
 ffplay -vf "drawtext=text='%{pts}':fontsize=60:fontcolor=blue:x=10:y=10" \
        -fflags nobuffer -flags low_delay -framedrop -sync ext \
-       -protocol_whitelist "file,udp,rtp" mjpeg.sdp
+       -protocol_whitelist "file,udp,rtp" mjpg.sdp
